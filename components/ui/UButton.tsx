2b24d4489c476db6fc9901882920c53157a3ec0c
gui117 <48198890+gui117@users.noreply.github.com>
2024-11-03 11:45:11 +0800

    feat: change button style.

--- components/ui/UButton.tsx
+++ components/ui/UButton.tsx
@@ -8,8 +8,8 @@ export interface BaseButtonProps extends ButtonProps {
 
 export default function UButton({ title, onPress, ...props }: BaseButtonProps) {
   return (
-    <Pressable {...props} className="items-center justify-center py-2 px-5 bg-primary rounded-[50] active:opacity-80" onPress={onPress}>
-      <Text className="text-lg text-[white] font-bold">{title}</Text>
+    <Pressable {...props} className="items-center justify-center py-2 px-5 bg-card rounded-full active:opacity-80" onPress={onPress}>
+      <Text className="text-lg text-primary font-bold select-none">{title}</Text>
     </Pressable>
   )
 }
